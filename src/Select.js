e357bffbcc43dce2fe30088e21590b0f9b2cf425
mcongost <mcongost@guellconsulting.com>
2021-01-03 12:25:03 +0100

    Use a custom component for Select, instead of the native control

--- src/Select.js
+++ src/Select.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
 import { ParameterError, ParameterSet } from "./utils";
+import { Dropdown } from "./Dropdown";
+
+let selectCount = 0;
 
 export function Select(props) {
+
     const parameters = new ParameterSet(props, "flg-Select");
     const field = parameters.pop("field", null);
     const selectedValue = parameters.pop("value", null);
     const selectedValueId = field ? field.getValueId(selectedValue) : selectedValue;
     const onChange = parameters.pop("onChange", null);
-    const useUndefinedForBlank = parameters.pop("useUndefinedForBlank", false);
+    const nameRef = useRef(null);
+    const dropdownController = {};
+
+    if (!nameRef.current) {
+        nameRef.current = `flg-Select-${selectCount++}`;
+    }
+
+    let ItemComponent = parameters.pop("Item", null);
     let possibleValues = parameters.pop("possibleValues", null);
 
     if (possibleValues === null) {
@@ -28,26 +39,104 @@ export function Select(props) {
         possibleValues.unshift(null);
     }
 
-    function onSelectChange(e) {
+    if (!ItemComponent) {
+        ItemComponent = props => field ? field.getValueLabel(props.value) : props.value;
+    }
+
+    function selectElement(element) {
         if (onChange) {
+            const index = Number(element.getAttribute("data-index"));
             onChange({
                 field,
                 previousValue: selectedValue,
-                newValue: possibleValues[e.target.selectedIndex]
+                newValue: possibleValues[index]
             });
         }
     }
 
+    function handleSelectionChanged(e) {
+        dropdownController.close();
+        selectElement(e.target.parentNode.parentNode);
+    }
+
+    function handleKeyDown(e) {
+
+        let nextSelection = null;
+        const selectedItem = (
+            e.target.parentNode.querySelector(".flg-Select-item[data-selected='true']")
+        );
+
+        if (e.key == "ArrowDown") {
+            if (selectedItem) {
+                let node = selectedItem.nextSibling;
+                while (node) {
+                    if (node.classList && node.classList.contains("flg-Select-item")) {
+                        nextSelection = node;
+                        break;
+                    }
+                    node = node.nextSibling;
+                }
+            }
+            else {
+                nextSelection = e.target.parentNode.querySelector(".flg-Select-item");
+            }
+        }
+        else if (e.key == "ArrowUp") {
+            if (selectedItem) {
+                let node = selectedItem.previousSibling;
+                while (node) {
+                    if (node.classList && node.classList.contains("flg-Select-item")) {
+                        nextSelection = node;
+                        break;
+                    }
+                    node = node.previousSibling;
+                }
+            }
+        }
+        else if (e.key == "Home") {
+            nextSelection = e.target.parentNode.querySelector(".flg-Select-item");
+        }
+        else if (e.key == "End") {
+            nextSelection = e.target.parentNode.querySelector(".flg-Select-item:last-child");
+        }
+
+        if (nextSelection) {
+            selectElement(nextSelection);
+        }
+    }
+
     return (
-        <select value={selectedValueId} onChange={onSelectChange} {...parameters.remaining}>
-            {possibleValues.map(possibleValue => {
-                const valueId = field.getValueId(possibleValue);
-                return (
-                    <option key={valueId} value={valueId}>
-                        {field ? field.getValueLabel(possibleValue) : possibleValue}
-                    </option>
-                );
-            })}
-        </select>
+        <Dropdown
+            buttonContent={field.getValueLabel(selectedValue)}
+            controller={dropdownController}
+            onKeyDown={handleKeyDown}
+            {...parameters.remaining}>
+            <ul className="flg-Select-list">
+                {possibleValues.map((possibleValue, index) => {
+                    const valueId = field.getValueId(possibleValue);
+                    const isSelected = valueId == selectedValueId;
+                    return (
+                        <li
+                            data-value={valueId}
+                            data-selected={isSelected}
+                            data-index={index}
+                            key={valueId}
+                            className="flg-Select-item">
+                            <label className="flg-Select-item-label">
+                                <input
+                                    className="flg-Select-item-input"
+                                    name={nameRef.current}
+                                    type="radio"
+                                    value={valueId}
+                                    checked={isSelected}
+                                    onChange={handleSelectionChanged}
+                                    tabIndex="-1"/>
+                                <ItemComponent value={possibleValue}/>
+                            </label>
+                        </li>
+                    );
+                })}
+            </ul>
+        </Dropdown>
     );
 }
